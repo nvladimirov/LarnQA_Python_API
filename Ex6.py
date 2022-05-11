d56e34819e4828747754f1e627823a00974ee6d8
n.vladimirov <n.vladimirov@corp.mail.ru>
2022-05-11 20:16:49 +0300

    Ex6

---
+++ Ex6.py
@@ -0,0 +1,10 @@
+import requests
+
+response = requests.post("https://playground.learnqa.ru/api/long_redirect")
+first = response.history[0]
+second = response.history[1]
+third = response
+
+print(first.url)
+print(second.url)
+print(third.url)
