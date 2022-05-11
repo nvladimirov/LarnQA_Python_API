b4ececacba16c92a238c836220048b84fd3dc498
n.vladimirov <n.vladimirov@corp.mail.ru>
2022-05-11 20:26:43 +0300

    Ex6

--- Ex6.py
+++ Ex6.py
@@ -1,6 +1,6 @@
 import requests
 
-response = requests.post("https://playground.learnqa.ru/api/long_redirect")
+response = requests.get("https://playground.learnqa.ru/api/long_redirect")
 first = response.history[0]
 second = response.history[1]
 third = response
@@ -8,3 +8,4 @@ third = response
 print(first.url)
 print(second.url)
 print(third.url)
+
